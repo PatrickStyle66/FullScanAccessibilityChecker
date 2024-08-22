88333493eacdc99d69b4a541044760dd6f630ba9
PatrickStyle66 <jphamega@hotmail.com>
2024-08-22 14:56:43 -0300

    refactor(Webdriver):Adapted the script to work with 3 tabs instead of 3 windows

--- main.py
+++ main.py
@@ -6,32 +6,37 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pyperclip
 driver = webdriver.Edge()
-AccessDriver = webdriver.Edge()
-htmlDriver = webdriver.Edge()
+driver.set_window_position(-10000,0)
+driver.switch_to.new_window('tab')
+driver.switch_to.new_window('tab')
 ScoresTable = {'Page':[],'Score':[]}
 flag = True
 def getPageScore(html):
-    AccessDriver.get('https://accessmonitor.acessibilidade.gov.pt/')
-    HtmlMode = WebDriverWait(AccessDriver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@data-rr-ui-event-key,"tab2")]')))
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get('https://accessmonitor.acessibilidade.gov.pt/')
+    HtmlMode = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@data-rr-ui-event-key,"tab2")]')))
     HtmlMode.click()
-    search = WebDriverWait(AccessDriver, 10).until(EC.presence_of_element_located((By.ID, "html")))
+    search = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "html")))
     search.clear()
     if flag:
+        driver.switch_to.window(driver.window_handles[0])
         pyperclip.copy(driver.page_source)
     else:
         pyperclip.copy(html)
+    driver.switch_to.window(driver.window_handles[1])
     search.send_keys(Keys.CONTROL, 'v')
     #WebDriverWait(AccessDriver, 60)
-    sendButton = WebDriverWait(AccessDriver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@id,"btn-html")]')))
+    sendButton = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@id,"btn-html")]')))
     sendButton.click()
     try:
-        score = WebDriverWait(AccessDriver, 60).until(
+        score = WebDriverWait(driver, 60).until(
             EC.presence_of_element_located((By.TAG_NAME, "svg")))
        # pageName = AccessDriver.find_element(By.CLASS_NAME,"resume_info_about_uri d-flex flex-column gap-4")
         #print(pageName.text)
         score = str(score.text).split('\n')[1]
         if not flag:
-            ScoresTable['Page'].append(htmlDriver.title)
+            driver.switch_to.window(driver.window_handles[2])
+            ScoresTable['Page'].append(driver.title)
         print(f'score da página: {score}')
         return float(score)
     except:
@@ -43,6 +48,7 @@ def getWebsiteScores(site):
                    'museus.cultura.gov.br', '.png', '.jpg', 'linkedin', 'mailto', 'wikipedia']
     unique = []
     try:
+        driver.switch_to.window(driver.window_handles[0])
         driver.get(site)
         site = driver.current_url
         print(f'domínio real: {site}')
@@ -54,6 +60,7 @@ def getWebsiteScores(site):
     points = 0
     count = 0
     result = getPageScore(site)
+    driver.switch_to.window(driver.window_handles[0])
     ScoresTable['Score'].append(result)
     unique.append(site)
     print(result)
@@ -76,9 +83,11 @@ def getWebsiteScores(site):
                if social in str(link).lower():
                    skip = True
            if not skip:
-             htmlDriver.get(link)
-             html = htmlDriver.page_source
+             driver.switch_to.window(driver.window_handles[2])
+             driver.get(link)
+             html = driver.page_source
              result = getPageScore(html)
+             driver.switch_to.window(driver.window_handles[0])
              unique.append(link)
              ScoresTable['Score'].append(result)
              print(result)
@@ -100,7 +109,7 @@ def getWebsiteScores(site):
     df2 = df.dropna()
     df2.to_csv('WebsiteScores.csv')
     driver.quit()
-    AccessDriver.quit()
+
 
 site = input("digite a url a ser analisada:")
 getWebsiteScores(site)
